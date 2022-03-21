961a6da0371708d298387ba9d41960fbc04d6aa0
Zakariyya Mughal <zaki.mughal@gmail.com>
2022-03-21 19:59:44 -0400

    Re-arrange POD

--- lib/Alien/7zip.pm
+++ lib/Alien/7zip.pm
@@ -5,6 +5,28 @@ use warnings;
 use base qw( Alien::Base );
 use 5.008004;
 
+=head1 NAME
+
+Alien::7zip - Find or build 7-Zip
+
+=head1 SYNOPSIS
+
+Command line tool:
+
+ use Alien::7zip;
+ use Env qw( @PATH );
+
+ unshift @PATH, Alien::7zip->bin_dir;
+
+=head1 DESCRIPTION
+
+This distribution provides 7-Zip so that it can be used by other
+Perl distributions that are on CPAN.  It does this by first trying to
+detect an existing install of 7-Zip on your system.  If found it
+will use that.  If it cannot be found, the source code will be downloaded
+from the internet and it will be installed in a private share location
+for the use of other modules.
+
 =head1 METHODS
 
 =head2 exe
@@ -40,28 +62,6 @@ sub alien_helper {
 }
 
 1;
-
-=head1 NAME
-
-Alien::7zip - Find or build 7-Zip
-
-=head1 SYNOPSIS
-
-Command line tool:
-
- use Alien::7zip;
- use Env qw( @PATH );
-
- unshift @PATH, Alien::7zip->bin_dir;
-
-=head1 DESCRIPTION
-
-This distribution provides 7-Zip so that it can be used by other
-Perl distributions that are on CPAN.  It does this by first trying to
-detect an existing install of 7-Zip on your system.  If found it
-will use that.  If it cannot be found, the source code will be downloaded
-from the internet and it will be installed in a private share location
-for the use of other modules.
 
 =head1 SEE ALSO
 
